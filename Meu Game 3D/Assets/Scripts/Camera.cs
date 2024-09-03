548bc9cee6d33eebe2c96b938134c2b1c3a632fb
PedroPauloZS <cruz.paulo@escolar.ifrn.edu.br>
2024-09-03 14:37:33 -0300

    Camera player

--- Meu Game 3D/Assets/Scripts/Camera.cs
+++ Meu Game 3D/Assets/Scripts/Camera.cs
@@ -17,6 +17,7 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = alvo.position - offset;
+
     }
 }
--- Meu Game 3D/Assets/Scripts/Player.cs
+++ Meu Game 3D/Assets/Scripts/Player.cs
@@ -7,14 +7,16 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     public int velocidade = 10;
+    
     Rigidbody rb;
 
-    public int forcaPulo = 10;
+    public int forcaPulo = 7;
 
     public bool noChao = false;
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("START");
         TryGetComponent(out rb);
     }
 
@@ -29,18 +31,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("UPDATE");
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         Vector3 direcao = new Vector3(h, 0, v);
         rb.AddForce(direcao * (velocidade * Time.deltaTime), ForceMode.Impulse);
         
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && noChao)
         {
             rb.AddForce(Vector3.up * forcaPulo,ForceMode.Impulse);
             noChao = false;
         }
-        if (transform.position.y < -5)
+        
+        
+        
+        if (transform.position.y <= -10)
         {
+            //jogador caiu
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
